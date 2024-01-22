c50f9f47c37515c2238123badf90a3894597b83e
Nikhil-Wadatkar-Basket <wadatkarnikhil22@gmail.com>
2024-01-22 17:39:26 +0530

    backend and front end service added

---
+++ App10_EmployeeCURDApp/src/main/java/com/sit/App10EmployeeCurdAppApplication.java
@@ -0,0 +1,13 @@
+package com.sit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App10EmployeeCurdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App10EmployeeCurdAppApplication.class, args);
+	}
+
+}
---
+++ App10_EmployeeCURDApp/src/main/java/com/sit/helper/Validator.java
@@ -0,0 +1,38 @@
+package com.sit.helper;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import com.sit.bean.Employee;
+import com.sit.constants.Messages;
+import com.sit.exception.InvalidEmployeeDetailsException;
+
+@Component
+public class Validator {
+
+	public void validateEmployeeDetails(Employee employeeDetails) {
+
+		if (employeeDetails == null)
+			throw new InvalidEmployeeDetailsException(Messages.EMPTY_EMPLOYEE_OBJECT);
+
+		if (StringUtils.isEmpty(employeeDetails.getName()))
+			throw new InvalidEmployeeDetailsException(Messages.EMPLOYEE_INVALID_NAME);
+		if (employeeDetails.getContact() < 1000000000L)
+			throw new InvalidEmployeeDetailsException(Messages.EMPLOYEE_INVLAID_CONTACT);
+		if (employeeDetails.getAge() == null)
+			throw new InvalidEmployeeDetailsException(Messages.EMPLOYEE_INVALID_AGE);
+		if (employeeDetails.getAge() <= 0)
+			throw new InvalidEmployeeDetailsException(Messages.EMPLOYEE_AGE_GREATER_THAN_ZERO);
+
+		if (StringUtils.isEmpty(employeeDetails.getEmail()))
+			throw new InvalidEmployeeDetailsException(Messages.EMPLOYEE_INVALID_EMAIL);
+
+		if (!employeeDetails.getEmail().contains(Messages.EMAIL_TEMPLATE))
+			throw new InvalidEmployeeDetailsException(Messages.EMPLOYEE_INVALID_EMAIL_TEMPLATE);
+//		if (employeeDetails.getSalary() == null || employeeDetails.getSalary() <= 0)
+//			throw new InvalidEmployeeDetailsException(Messages.EMPLOYEE_INVALID_SALARY);
+		if (StringUtils.isEmpty(employeeDetails.getQualification()))
+			throw new InvalidEmployeeDetailsException(Messages.EMPLOYEE_INVALID_QUALIFICATION);
+
+	}
+}
